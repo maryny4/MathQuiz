ce284535bd047fcd2b2c05d4d1242ed6ddf9d532
maryny4 <maryny4.original@gmail.com>
2023-10-22 19:46:45 +0200

    Add GameAnalytics

--- MathQuiz/Assets/Scripts/LoadingSceneController.cs
+++ MathQuiz/Assets/Scripts/LoadingSceneController.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using GameAnalyticsSDK;
 
 public class LoadingSceneController : MonoBehaviour
 {
     [SerializeField] private CanvasGroup splashScreen;
     IEnumerator Start()
     {
+        GameAnalytics.Initialize();
         splashScreen.transform.DOScale(1.2f, 2.5f);
         splashScreen.DOFade(0, 1.5f).SetDelay(1);
         yield return new WaitForSeconds(2.5f);
